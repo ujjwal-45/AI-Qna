afcd4d7431f6268683084f9794fd0248f8ce4128
Ujjwal Choudhary <106880198+ujjwal-45@users.noreply.github.com>
2023-09-08 21:16:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain import OpenAI, VectorDBQA
-from langchain.chains import RetrievelQAWithSourcesChain
+from langchain.chains import RetrievalQAWithSourcesChain
 import PyPDF2
 
 # function to extract pdf pages return list of page texts
